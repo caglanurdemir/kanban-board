d330b66f011eccd6981c391b8437db120e350385
caglanurdemir <nurcagla.demir@gmail.com>
2023-05-23 23:39:46 +0300

    css fix for home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './components/Header';
 import KanbanBoard from './components/KanbanBoard';
+import Sidebar from './components/Sidebar';
+import './App.scss';
 
-function App() {
+const App = () => {
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
+
+  const handleToggleSidebar = () => {
+    setSidebarCollapsed(!sidebarCollapsed);
+  };
+
   return (
-    <div className="App">
-      <KanbanBoard />
+    <div className="app">
+      <Header />
+      <div className={`content ${sidebarCollapsed ? 'sidebar-collapsed' : ''}`}>
+        <Sidebar />
+        <KanbanBoard />
+        <button className="collapse-button" onClick={handleToggleSidebar}>
+          {sidebarCollapsed ? '>' : '<'}
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import kanbanReducer from './reducers/kanbanReducer';
-import './index.css';
+import './index.scss';
 import App from './App';
 
 const store = configureStore({
