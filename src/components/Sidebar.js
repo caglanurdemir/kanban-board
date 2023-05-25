b104d922339cca3d30fbca4d340c16f69663fbda
caglanurdemir <nurcagla.demir@gmail.com>
2023-05-25 23:54:00 +0300

    finalize the sidebar css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/Sidebar.scss';
 import dashboard from '../assets/sidebarIcons/dashboard.svg';
 import chat from '../assets/sidebarIcons/chat.svg';
-import home from '../assets/sidebarIcons/home-alt.svg';
+import task from '../assets/sidebarIcons/task.svg';
 import explore from '../assets/sidebarIcons/explore.svg';
 import repeat from '../assets/sidebarIcons/repeat.svg';
 import bell from '../assets/sidebarIcons/bell.svg';
@@ -10,59 +10,56 @@ import company from '../assets/sidebarIcons/company.svg';
 import name from '../assets/sidebarIcons/name.svg';
 import help from '../assets/sidebarIcons/chat-alt.svg';
 import plus from '../assets/sidebarIcons/plus.svg';
+import line from '../assets/sidebarIcons/line.svg';
 
 const Sidebar = () => {
-    // const [collapsed, setCollapsed] = useState(false);
-
-    // const handleToggleCollapse = () => {
-    //     setCollapsed(!collapsed);
-    // };
-
     return (
         <div className={`sidebar`}>
             <div className="sidebar-top">
                 <div className="sidebar-buttons">
                     <button className="sidebar-button">
-                        <img src={dashboard} alt="Dashboard" />
+                        <img src={dashboard} alt="Dashboard" className='icons' />
                         Dashboard
                     </button>
                     <button className="sidebar-button">
-                        <img src={chat} alt="Feedback" />
+                        <img src={chat} alt="Feedback" className='icons' />
                         Feedback
                     </button>
                     <button className="sidebar-button">
-                        <img src={home} alt="Task" />
+                        <img src={task} alt="Task" className='icons' />
                         Task
                     </button>
                     <button className="sidebar-button">
-                        <img src={explore} alt="Roadmap" />
+                        <img src={explore} alt="Roadmap" className='icons' />
                         Roadmap
                     </button>
                     <button className="sidebar-button">
-                        <img src={repeat} alt="Changelog" />
+                        <img src={repeat} alt="Changelog" className='icons' />
                         Changelog
                     </button>
                 </div>
             </div>
             <div className="sidebar-bottom">
                 <button className="sidebar-button">
-                    <img src={plus} alt="Invite People" />
+                    <img src={plus} alt="Invite People" className='icons' />
                     Invite People
                 </button>
                 <button className="sidebar-button">
-                    <img src={help} alt="Help Community" />
+                    <img src={help} alt="Help Community" className='icons' />
                     Help Community
                 </button>
-                <button className="sidebar-button">
-                    <img src={bell} alt="Notifications" />
+                <button className="sidebar-button-user">
+                    <img src={bell} alt="Notifications" className='icons' />
                     Notifications
                 </button>
-                <button className="sidebar-button">
-                    <img src={name} alt="Name Surname" />
+                <img src={line} alt="Line" className='line' />
+                <button className="sidebar-button-user">
+                    <img src={name} alt="Name Surname" className='user-logo' />
                     Name Surname
                 </button>
-                <button className="sidebar-button">
-                    <img src={company} alt="Company Logo" />
+                <img src={line} alt="Line" className='line' />
+                <button className="sidebar-button-user">
+                    <img src={company} alt="Company Logo" className='user-logo' />
                     Company
                 </button>
             </div>
